e8c0598aebb330784873e5b3373c7d6bbea22845
bobbydu <bobbydu.mar31@gmail.com>
2023-07-30 17:38:19 +0800

    Updated deployment folder

--- deployment/bert/lambda_function.py
+++ deployment/bert/lambda_function.py
@@ -3,7 +3,7 @@ import tensorflow_text as text
 from keras.models import load_model
 import json
 
-model = load_model('saved_model/bertSmsClassifier',compile=False)
+model = load_model('saved_model/final_dataset_bert_en_uncased_L-8_H-512_A-8',compile=False)
 
 def predict(features):
     return model.predict(features).tolist()
